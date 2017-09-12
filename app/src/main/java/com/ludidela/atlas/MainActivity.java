f1fdcaeff040742665aecb30ecd71066ac2564c1
seogras <sagelsas@gmail.com>
2017-09-13 00:34:14 +0300

    Initial commit

---
+++ app/src/main/java/com/ludidela/atlas/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ludidela.atlas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
